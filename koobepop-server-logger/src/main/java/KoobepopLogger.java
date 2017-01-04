fec2fcb60e5b80b9750aa0b68bf8cdce54053259
amelcon <amelcon@biit-solutions.com>
2017-01-04 15:33:49 +0100

    Second commit Poms :)

---
+++ koobepop-server-logger/src/main/java/KoobepopLogger.java
@@ -0,0 +1,82 @@
+import org.apache.log4j.Logger;
+
+import com.biit.logger.BiitLogger;
+
+
+public class KoobepopLogger extends BiitLogger {
+	
+	private static Logger logger = Logger.getLogger(KoobepopLogger.class);
+	
+	/**
+	 * Events that have business meaning (i.e. creating category, deleting form,
+	 * ...). To follow user actions.
+	 */
+	public static void info(String className, String message) {
+		info(logger, className, message);
+	}
+
+	/**
+	 * Shows not critical errors. I.e. Email address not found, permissions not
+	 * allowed for this user, ...
+	 * 
+	 * @param message
+	 */
+	public static void warning(String className, String message) {
+		warning(logger, className, message);
+	}
+
+	/**
+	 * For following the trace of the execution. I.e. Knowing if the application
+	 * access to a method, opening database connection, etc.
+	 */
+	public static void debug(String className, String message) {
+		debug(logger, className, message);
+	}
+
+	/**
+	 * To log any not expected error that can cause application malfunction.
+	 * 
+	 * @param message
+	 */
+	public static void severe(String className, String message) {
+		severe(logger, className, message);
+	}
+
+	/**
+	 * To log java exceptions and log also the stack trace. If enabled, also can
+	 * send an email to the administrator to alert of the error.
+	 * 
+	 * @param className
+	 * @param throwable
+	 */
+	public static void errorMessage(String className, Throwable throwable) {
+		errorMessageNotification(logger, className, getStackTrace(throwable));
+	}
+
+	public static void errorMessage(Class<?> clazz, Throwable throwable) {
+		errorMessageNotification(logger, clazz.getName(), getStackTrace(throwable));
+	}
+
+	/**
+	 * To log java exceptions and log also the stack trace. If enabled, also can
+	 * send an email to the administrator to alert of the error.
+	 * 
+	 * @param className
+	 * @param error
+	 */
+	public static void errorMessage(String className, String error) {
+		errorMessageNotification(logger, className, error);
+	}
+
+	public static void errorMessage(Object object, Throwable throwable) {
+		errorMessageNotification(logger, object.getClass().getName(), getStackTrace(throwable));
+	}
+
+	public static void sendNotification(String className, String message) {
+		sendNotification(logger, className, message);
+	}
+	
+
+
+
+}
