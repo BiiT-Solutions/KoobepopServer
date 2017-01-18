f37dc1d4b334dcc00457dbc1542d1226c2b1f592
amelcon <amelcon@biit-solutions.com>
2017-01-18 15:19:44 +0100

    Vaadin is coming home

---
+++ koobepop-server-gui/src/main/java/com/biit/koobepopserver/gui/login/KoobepopLoginPresenter.java
@@ -0,0 +1,79 @@
+package com.biit.koobepopserver.gui.login;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.biit.koobepopserver.core.security.ISecurityService;
+import com.biit.vaadin.webpages.login.ILoginPresenter;
+import com.biit.vaadin.webpages.login.LoginPresenter;
+import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.spring.annotation.UIScope;
+
+@UIScope
+@SpringComponent
+public class KoobepopLoginPresenter extends LoginPresenter implements ILoginPresenter {
+
+	@Autowired
+	private ISecurityService koobepopSecurityService;
+
+	@Override
+	public String getLoginCaption() {
+		return "";// LanguageCode.LOGIN_CAPTION_SIGN_IN.translation();
+	}
+
+	@Override
+	public String getEmailCaption() {
+		return "";// LanguageCode.LOGIN_CAPTION_EMAIL.translation();
+	}
+
+	@Override
+	public String getPasswordCaption() {
+		return "";// LanguageCode.LOGIN_CAPTION_PASSWORD.translation();
+	}
+
+	@Override
+	public String getUsernameRequiredErrorCaption() {
+		return "";// LanguageCode.LOGIN_ERROR_EMAIL.translation();
+	}
+
+	@Override
+	public String getPasswordRequiredErrorCaption() {
+		return "";// LanguageCode.LOGIN_ERROR_PASSWORD.translation();
+	}
+
+	@Override
+	public String getNameVersion() {
+		return "";// LanguageCode.LOGIN_SCREEN_SYSTEM_NAME.translation(getCastedView().getVersion());
+	}
+
+	@Override
+	public void checkUserAndPassword(String username, String password) {
+		if (username == null || username.isEmpty()) {
+			getCastedView().getUsernameField().setRequiredError(getUsernameRequiredErrorCaption());
+		}
+		if (password == null || password.isEmpty()) {
+			getCastedView().getPasswordField().setRequiredError(getPasswordRequiredErrorCaption());
+		}
+
+		// try {
+		// koobepopSecurityService.login(username, password);
+		// UserSessionHandler.navigateToUserMainView();
+		// } catch (InvalidCredentialsException e) {
+		// getCastedView().setPasswordError(LanguageCode.LOGIN_ERROR_USER.translation(username));
+		// UsmoMessageManager.showError(LanguageCode.ERROR_BADUSERPSWD,
+		// LanguageCode.ERROR_TRYAGAIN);
+		// } catch (UserManagementException | AuthenticationRequired e) {
+		// UsmoLogger.errorMessage(this.getClass().getName(), e);
+		// UsmoMessageManager.showError(LanguageCode.ERROR_USER_SERVICE,
+		// LanguageCode.ERROR_CONTACT);
+		// } catch (InsufficientPermissions e) {
+		// UsmoMessageManager.showError(LanguageCode.ERROR_INSUFFICIENT_PERMISSIONS,
+		// LanguageCode.ERROR_CONTACT);
+		// }
+	}
+
+	@Override
+	public void init() {
+		// Do nothing
+	}
+
+}
---
+++ koobepop-server-gui/src/main/java/com/biit/koobepopserver/gui/webpages/KoobepopLogin.java
@@ -0,0 +1,32 @@
+package com.biit.koobepopserver.gui.webpages;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.biit.vaadin.mvp.MVPVaadinView;
+import com.biit.vaadin.webpages.login.ILoginPresenter;
+import com.biit.vaadin.webpages.login.ILoginView;
+import com.biit.vaadin.webpages.login.LoginView;
+import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.spring.annotation.UIScope;
+
+@UIScope
+@SpringView(name = KoobepopLogin.NAME)
+public class KoobepopLogin extends MVPVaadinView<ILoginView, ILoginPresenter>{
+	private static final long serialVersionUID = 8674023750326851914L;
+
+	public static final String NAME="name";
+
+	@Autowired
+	private ILoginPresenter koobepopLoginPresenter;
+
+	@Override
+	protected ILoginPresenter instanciatePresenter() {
+		return koobepopLoginPresenter;
+	}
+
+	@Override
+	protected ILoginView instanciateView() {
+		return new LoginView();
+	}
+	
+}
